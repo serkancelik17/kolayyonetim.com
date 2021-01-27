13957861e0ad697f63f482f02e494149f5944d20
Serkan Celik <syaramaz@gmail.com>
2021-01-27 21:26:48 +0300

    initial commit

---
+++ Modules/User/Resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('user::layouts.master')
+
+@section('content')
+    <h1>user-profile sayfası</h1>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<h1>Ana Sayfa</h1>
+<h2>Giriş ekranı bilgileri</h2>
+<a href="/">Giriş Yap</a>
